e9af0ecfe0d6592dba56f92ba5239a322fd35f24
ohchance2 <ohchance2@gmail.com>
2025-05-19 18:08:43 +0900

    opencv 추가

---
+++ opencv/build/intermediates/cxx/Debug/5z1i6643/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\USER\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\USER\\StudioProjects\\OCR\\opencv\\.cxx\\Debug\\5z1i6643\\arm64-v8a" ^
+  opencv_jni_shared
