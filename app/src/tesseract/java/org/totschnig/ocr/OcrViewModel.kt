6744ee4c1d99f8bb74422221eb9fba5f3590df5f
ohchance2 <ohchance2@gmail.com>
2025-05-19 16:58:36 +0900

    opencv 추가

--- app/src/tesseract/java/org/totschnig/ocr/OcrViewModel.kt
+++ app/src/tesseract/java/org/totschnig/ocr/OcrViewModel.kt
@@ -24,6 +24,11 @@ import java.io.File
 import java.util.*
 import androidx.core.net.toUri
 
+import org.opencv.core.Mat
+import org.opencv.core.Size
+import org.opencv.imgproc.Imgproc
+import org.opencv.android.Utils
+
 const val TESSERACT_DOWNLOAD_FOLDER = "tesseract4/fast/"
 
 class OcrViewModel(application: Application) : BaseViewModel(application) {
@@ -31,6 +36,42 @@ class OcrViewModel(application: Application) : BaseViewModel(application) {
 
     val preferences: SharedPreferences
         get() = PreferenceManager.getDefaultSharedPreferences(getApplication())
+
+    fun preprocessImage(image: Bitmap): Bitmap {
+        val matImage = Mat()
+        Utils.bitmapToMat(image, matImage)
+
+        val scaledMat = Mat()
+        Imgproc.resize(matImage, scaledMat, Size(), 2.0, 2.0, Imgproc.INTER_LINEAR)
+
+        // 그레이스케일로 변환
+        Imgproc.cvtColor(scaledMat, scaledMat, Imgproc.COLOR_RGB2GRAY)
+
+        // 대비 향상 (Contrast adjustment)
+        val equalizedMat = Mat()
+        Imgproc.equalizeHist(scaledMat, equalizedMat)
+
+        // 가우시안 블러
+        val blurredMat = Mat()
+        Imgproc.GaussianBlur(equalizedMat, blurredMat, Size(5.0, 5.0), 0.0)
+
+        // 로컬 이진화 (Adaptive Thresholding)
+        val adaptiveThresholdMat = Mat()
+        Imgproc.adaptiveThreshold(
+            blurredMat,  // 입력 이미지 (그레이스케일 및 대비 향상 처리된 이미지)
+            adaptiveThresholdMat,  // 출력 이미지 (이진화)
+            255.0,  // 최대값 (픽셀 값의 최대값)
+            Imgproc.ADAPTIVE_THRESH_MEAN_C,  // 임계값 계산 방식
+            Imgproc.THRESH_BINARY,  // 이진화 방식
+            11,  // 블록 크기 (임계값 계산에 사용할 이웃 영역 크기)
+            2.0  // 하위 임계값 조정 (블록 내 평균값에 추가할 값)
+        )
+
+        val resultBitmap = Bitmap.createBitmap(adaptiveThresholdMat.cols(), adaptiveThresholdMat.rows(), Bitmap.Config.ARGB_8888)
+        Utils.matToBitmap(adaptiveThresholdMat, resultBitmap)
+
+        return resultBitmap
+    }
 
     fun runTextRecognition(uri: Uri) {
         viewModelScope.launch {
@@ -56,7 +97,7 @@ class OcrViewModel(application: Application) : BaseViewModel(application) {
                                         File(
                                             application.getExternalFilesDir(null),
                                             TESSERACT_DOWNLOAD_FOLDER
-                                        ).path, language()
+                                        ).path, "eng+" + language()
                                     )
                                 ) {
                                     throw IllegalStateException("Could not init Tesseract")
@@ -70,12 +111,15 @@ class OcrViewModel(application: Application) : BaseViewModel(application) {
                                 setVariable("load_bigram_dawg", TessBaseAPI.VAR_FALSE)
                                 setVariable("load_fixed_length_dawgs", TessBaseAPI.VAR_FALSE)
                                 pageSegMode = TessBaseAPI.PageSegMode.PSM_AUTO_OSD
+                                /*
                                 setImage(with(FastBitmap(it.rotate(getOrientation(uri)))) {
                                     toGrayscale()
                                     val g: IApplyInPlace = BradleyLocalThreshold()
                                     g.applyInPlace(this)
                                     toBitmap()
                                 })
+                                */
+                                setImage(preprocessImage(FastBitmap(it.rotate(getOrientation(uri))).toBitmap()))
                                 utF8Text
                                 val lines = mutableListOf<Line>()
                                 with(resultIterator) {
@@ -141,7 +185,8 @@ class OcrViewModel(application: Application) : BaseViewModel(application) {
 
     fun downloadTessData(context: Context) = language()?.let {
         val uri =
-            "https://github.com/tesseract-ocr/tessdata_fast/raw/4.0.0/${fileName(it)}".toUri()
+            //"https://github.com/tesseract-ocr/tessdata_fast/raw/4.0.0/${fileName(it)}".toUri()
+            "https://github.com/tesseract-ocr/tessdata_best/raw/main/${fileName(it)}".toUri()
         ContextCompat.getSystemService(context, DownloadManager::class.java)?.enqueue(
             DownloadManager.Request(uri)
                 .setTitle(
